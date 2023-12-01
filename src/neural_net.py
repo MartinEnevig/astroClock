99b5cda665d9058017f3d6bc59d13bcc8dde8a38
Martin Enevig Clausen <marcla@erst.dk>
2023-12-01 11:55:06 +0100

    loss function done

--- src/neural_net.py
+++ src/neural_net.py
@@ -1,17 +1,20 @@
 import numpy as np
-from keras.layers import Dense
+from keras.layers import Dense, Normalization
 from keras import Input, Model
 import keras.backend as K
+from keras.models import load_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow import cast, greater_equal
 from tensorflow.python.framework.ops import disable_eager_execution
-import astroClock.src.utils.const as constants
-
+import src.utils.const as constants
+from src.utils.collision import collisionCheck, euclidian_dist
+import tensorflow as tf
+from typing import Tuple
 disable_eager_execution()
+print(tf.__version__)
 
-
-class NeuralClock_keras:
+class NeuralClock:
     
     def __init__(self) -> None:
         with open("./src/data/training_data.npy", "rb") as train:
@@ -20,50 +23,60 @@ class NeuralClock_keras:
             self.target_data = np.load(target)
 
         self.full_data = np.hstack((self.training_data, self.target_data))
+        self.scaler = StandardScaler()
 
-    def make_model(self) -> Model:
-
+    def make_model(self, training_data: np.ndarray) -> Model:
+        mean, variance = self._get_mean_and_variance(training_data=training_data)
+        normalizer = Normalization(mean=mean, variance=variance)
         input_layer = Input(shape=(4, ))
-        dense_1 = Dense(units=10, kernel_initializer="he_normal", activation="relu")(input_layer)
-        dense_2 = Dense(units=10, kernel_initializer="he_normal", activation="relu")(dense_1)
+        normalized = normalizer(input_layer)
+        dense_1 = Dense(units=12, kernel_initializer="he_normal", activation="relu")(normalized)
+        dense_2 = Dense(units=12, kernel_initializer="he_normal", activation="relu")(dense_1)
         dense_3 = Dense(units=10, kernel_initializer="he_normal", activation="relu")(dense_2)
         dense_4 = Dense(units=10, kernel_initializer="he_normal", activation="relu")(dense_3)
-        output_layer = Dense(units=2)(dense_4)
+        dense_5 = Dense(units=8, kernel_initializer="he_normal", activation="relu")(dense_4)
+        dense_6 = Dense(units=6, kernel_initializer="he_normal", activation="relu")(dense_5)
+        output_layer = Dense(units=2)(dense_6)
         model = Model(input_layer, output_layer)
         model.compile(loss=self.loss_wrapper(input_layer), optimizer="adam")
         return model
 
+    @staticmethod
+    def _get_mean_and_variance(training_data: np.ndarray) -> Tuple:
+        mean = np.mean(training_data[:, 0:4], axis=0)
+        variance = np.var(training_data[:, 0:4], axis=0)
 
-    def splitter(self, data: np.ndarray) -> tuple:
+        return mean, variance
+
+    
+    def splitter(self, data: np.ndarray, scale: bool=False) -> tuple:
         train, val_test = train_test_split(data, test_size=0.2, random_state=42)
         val, test = train_test_split(val_test, test_size=0.5, random_state=42)
-
-        scaler = StandardScaler()
-        scaler.fit(X=train)
-        train = scaler.transform(X=train)
-        val = scaler.transform(X=val)
-        test = scaler.transform(X=test)
+        if scale:
+            self.scaler.fit(X=train)
+            train = self.scaler.transform(X=train)
+            val = self.scaler.transform(X=val)
+            test = self.scaler.transform(X=test)
 
         return train, val, test
     
 
     def normalize(self, data, fit: bool = False):
-        scaler = StandardScaler()
         if fit:
-            scaler.fit(X=data)
-        return scaler.transform(X=data)
+            self.scaler.fit(X=data)
+        return self.scaler.transform(X=data)
+    
 
-    @staticmethod
-    def loss_wrapper(input):
+    def loss_wrapper(self, input):
         def custom_loss(y_true, y_pred):
             input_tensor = input[:, 0:2]
             full_prediction = input_tensor + y_pred
-            y_true_cartesian = K.stack([y_true[:, 0]*K.cos(y_true[:, 1]), y_true[:, 0]*K.sin(y_true[:, 1])], axis=-1) 
-            y_pred_cartesian = K.stack([full_prediction[:, 0]*K.cos(full_prediction[:, 1]), full_prediction[:, 0]*K.sin(full_prediction[:, 1])], axis=-1)
+
+            euclidian_distance = euclidian_dist(pos1=y_true, pos2=full_prediction)
+
+            board_edge_penalty = cast(greater_equal(full_prediction[:, 0], constants.BOARD_RADIUS - constants.sun["MarkerRadius"]), dtype=np.float32)*10_000
+
+            collision_penalty = cast(greater_equal(collisionCheck(input), 1), dtype=np.float32)*10_000
             
-            euclidian_distance = K.sqrt(K.sum(K.square(y_pred_cartesian - y_true_cartesian), axis=-1))
-
-            board_edge_penalty = cast(greater_equal(full_prediction[:, 0], constants.BOARD_RADIUS - constants.sun["MarkerRadius"]), dtype=np.float32)*1000
-
-            return euclidian_distance + board_edge_penalty
+            return K.square(euclidian_distance) + board_edge_penalty + collision_penalty
         return custom_loss
--- src/utils/collision.py
+++ src/utils/collision.py
@@ -1,86 +1,46 @@
+import keras.backend as K
+from tensorflow import cast, less_equal
 import numpy as np
-from typing import Optional, Literal, Union
 
+def _calc_cartesian_pos(pos):
+    cart_pos = K.stack([pos[:, 0]*K.sin(pos[:, 1]), pos[:, 0]*K.cos(pos[:, 1])], axis=-1) 
+    return cart_pos
 
-def nearest_objects(
-        current_object_pos: np.ndarray,
-        object_0_pos: np.ndarray,
-        object_1_pos: np.ndarray,
-        object_2_pos: np.ndarray,
-        object_3_pos: np.ndarray,
-        object_4_pos: np.ndarray,
-        object_5_pos: np.ndarray,
-        object_6_pos: np.ndarray,
-        object_7_pos: np.ndarray,
-        object_8_pos: np.ndarray,
-) -> np.ndarray:
-    """
-    A function to sort the bodies by how close they are to the current body. is going to be used to create the training dataset, 
-    and to feed data to the model in production. Maybe just an argsort on theta, or maybe that is not enough? We could perhaps get a situation 
-    where objects are close on the theta parameter but distant enough on r?
-    """
-    posArray = np.empty(9, dtype=np.float32)
-    posArray[0] = object_0_pos[1] 
-    posArray[1] = object_1_pos[1] 
-    posArray[2] = object_2_pos[1] 
-    posArray[3] = object_3_pos[1] 
-    posArray[4] = object_4_pos[1] 
-    posArray[5] = object_5_pos[1] 
-    posArray[6] = object_6_pos[1] 
-    posArray[7] = object_7_pos[1] 
-    posArray[8] = object_8_pos[1] 
+def euclidian_dist(pos1, pos2):
+    pos_1_cart = _calc_cartesian_pos(pos1)
+    pos_2_cart = _calc_cartesian_pos(pos2)
 
-    for i in range(9):
-        posArray[i] -= current_object_pos[1]
-        if posArray[i] < -np.pi : posArray[i] += 2*np.pi
-        elif posArray[i] > np.pi : posArray[i] -= 2*np.pi
-    
+    dist = K.sqrt(K.sum(K.square(pos_1_cart - pos_2_cart), axis=-1))
+    return dist
 
-    return np.argsort(np.abs(posArray))
+def collisionCheck(pos1, pos2):
+    return (euclidian_dist(pos1, pos2) < 120) 
 
 def is_collision(
-        current_object_start_position: np.ndarray,
-        current_object_end_position: np.ndarray,
-        object_1_pos: np.ndarray,
-        object_2_pos: np.ndarray,
-        object_3_pos: np.ndarray,
-        object_4_pos: np.ndarray,
-        current_object_no: Optional[int],
-        object_1_no: Optional[int],
-        object_2_no: Optional[int],
-        object_3_no: Optional[int],
-        object_4_no: Optional[int], 
-        ) -> Union[Literal[0], Literal[1]]:
+        object_positions, 
+        ):
     """
     A function that takes the current position, its predicted end position and the positions of the nearest four objects as input. 
     From this the function should calculate if the predicted position would result in a collision - probably calculated both at the endpoint and midway.
     The function should return 1 if there is a colission and 0 if not. This is easier to use in the loss function than booleans.
 
     The object numbers are also included as optional parameters if we wish to do a calculation that takes the individual object's radius into acount.  
+    
+    Returns: A vector containing the sum of colissions.
     """
-    if collisionCheck(current_object_end_position, object_1_pos):
-        return 1
-    if collisionCheck(current_object_end_position, object_2_pos):
-        return 1  
-    if collisionCheck(current_object_end_position, object_3_pos):
-        return 1 
-    if collisionCheck(current_object_end_position, object_4_pos):
-        return 1
+    positions_length = K.eval(object_positions.shape[0])
+    
+    current_object_end_position = object_positions[:, 10:12]
+    dist_1 = euclidian_dist(current_object_end_position, object_positions[:, 2:4])
+    dist_2 = euclidian_dist(current_object_end_position, object_positions[:, 4:6])
+    dist_3 = euclidian_dist(current_object_end_position, object_positions[:, 6:8])
+    dist_4 = euclidian_dist(current_object_end_position, object_positions[:, 8:10])
 
-    return 0
+    
+    check_1 = cast(less_equal(x=dist_1, y=120), dtype=np.float32)
+    check_2 = cast(less_equal(x=dist_2, y=120), dtype=np.float32)
+    check_3 = cast(less_equal(x=dist_3, y=120), dtype=np.float32)
+    check_4 = cast(less_equal(x=dist_4, y=120), dtype=np.float32)
 
-def collisionCheck(pos1: np.ndarray, pos2: np.ndarray) -> bool:
-    # Simple one-size collision check
-    return (calcDistSquared(pos1, pos2) < 14400) # 14400 = 120mm ^2
-
-
-def calcCartesianPos(pos: np.ndarray)-> np.ndarray:
-    posXY = np.empty(2, dtype=np.float32)
-    posXY[0], posXY[1] = np.sin(pos[1])*pos[0] , np.cos(pos[1])*pos[0]
-    return posXY
-
-def calcDistSquared(pos1: np.ndarray, pos2: np.ndarray):
-    posXY1 = calcCartesianPos(pos1)
-    posXY2 = calcCartesianPos(pos2)
-    distSquared = (posXY1[0]-posXY2[0])**2 + (posXY1[1]-posXY2[1])**2
-    return distSquared
+    collision_tensor = K.stack((check_1, check_2, check_3, check_4), axis=1)
+    return K.reshape(K.sum(collision_tensor, axis=-1), (positions_length, 1))
