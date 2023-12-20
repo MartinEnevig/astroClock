f89b6befd5b3764f7679bc35a4bdf7c5c8c61227
Martin Enevig Clausen <marcla@erst.dk>
2023-12-20 10:16:47 +0100

    added training script

--- src/neural_net.py
+++ src/neural_net.py
@@ -2,33 +2,20 @@ import numpy as np
 from keras.layers import Dense, Normalization
 from keras import Input, Model
 import keras.backend as K
-from keras.models import load_model
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from tensorflow import cast, greater_equal
 from tensorflow.python.framework.ops import disable_eager_execution
 import src.utils.const as constants
-from src.utils.collision import collisionCheck, euclidian_dist
+from src.utils.collision import is_collision, euclidian_dist
 import tensorflow as tf
 from typing import Tuple
 disable_eager_execution()
-print(tf.__version__)
 
 class NeuralClock:
     
-    def __init__(self) -> None:
-        with open("./src/data/training_data.npy", "rb") as train:
-            self.training_data = np.load(train)
-        with open("./src/data/target_data.npy", "rb") as target:
-            self.target_data = np.load(target)
-
-        self.full_data = np.hstack((self.training_data, self.target_data))
-        self.scaler = StandardScaler()
-
     def make_model(self, training_data: np.ndarray) -> Model:
         mean, variance = self._get_mean_and_variance(training_data=training_data)
         normalizer = Normalization(mean=mean, variance=variance)
-        input_layer = Input(shape=(4, ))
+        input_layer = Input(shape=(10, ))
         normalized = normalizer(input_layer)
         dense_1 = Dense(units=12, kernel_initializer="he_normal", activation="relu")(normalized)
         dense_2 = Dense(units=12, kernel_initializer="he_normal", activation="relu")(dense_1)
@@ -43,29 +30,10 @@ class NeuralClock:
 
     @staticmethod
     def _get_mean_and_variance(training_data: np.ndarray) -> Tuple:
-        mean = np.mean(training_data[:, 0:4], axis=0)
-        variance = np.var(training_data[:, 0:4], axis=0)
+        mean = np.mean(training_data[:, 0:10], axis=0)
+        variance = np.var(training_data[:, 0:10], axis=0)
 
         return mean, variance
-
-    
-    def splitter(self, data: np.ndarray, scale: bool=False) -> tuple:
-        train, val_test = train_test_split(data, test_size=0.2, random_state=42)
-        val, test = train_test_split(val_test, test_size=0.5, random_state=42)
-        if scale:
-            self.scaler.fit(X=train)
-            train = self.scaler.transform(X=train)
-            val = self.scaler.transform(X=val)
-            test = self.scaler.transform(X=test)
-
-        return train, val, test
-    
-
-    def normalize(self, data, fit: bool = False):
-        if fit:
-            self.scaler.fit(X=data)
-        return self.scaler.transform(X=data)
-    
 
     def loss_wrapper(self, input):
         def custom_loss(y_true, y_pred):
@@ -76,7 +44,8 @@ class NeuralClock:
 
             board_edge_penalty = cast(greater_equal(full_prediction[:, 0], constants.BOARD_RADIUS - constants.sun["MarkerRadius"]), dtype=np.float32)*10_000
 
-            collision_penalty = cast(greater_equal(collisionCheck(input), 1), dtype=np.float32)*10_000
+            collision_penalty = cast(greater_equal(is_collision(object_positions=input, end_position=full_prediction), 1), dtype=np.float32)*10_000
             
             return K.square(euclidian_distance) + board_edge_penalty + collision_penalty
         return custom_loss
+    
--- src/train.py
+++ src/train.py
@@ -1,21 +1,38 @@
-import os
-from env import spaceEnv
+from src.neural_net import NeuralClock
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.callbacks import ModelCheckpoint
 
-from gymnasium.wrappers import flatten_observation
+def train_model(data_filepath: str, model_filepath: str):
 
-from stable_baselines3 import PPO, SAC
-from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.env_checker import check_env
+    with open(data_filepath, "rb") as file:
+        data = np.load(file)
+
+    training_data,  test_data = train_test_split(data, 0.2)
+    print(f"Generated training data with shape {training_data.shape} and test data with shape {test_data.shape}")
+    
+    network = NeuralClock()
+
+    model_checkpoint_callback = ModelCheckpoint(
+        filepath=model_filepath,
+        monitor="val_loss",
+        verbose=0,
+        save_best_only=True,
+        save_weights_only=True,
+        mode="min",
+    )
+
+    model = network.make_model(training_data)
+
+    model.fit(
+        x=training_data[:, 0:10], 
+        y=training_data[:, 10:12], 
+        epochs=100, 
+        verbose=1, 
+        initial_epoch=0, 
+        validation_split=0.2,
+        callbacks=[model_checkpoint_callback])
+
+    return model
 
 
-env =spaceEnv(render_mode=None)
-
-log_path = os.path.join('Training', 'Logs')
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_path)
-model.learn(total_timesteps=100000)
-
-
-model.save('PPO')
-
-evaluate_policy(model, env, n_eval_episodes=10, render=False)
